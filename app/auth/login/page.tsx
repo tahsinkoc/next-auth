5ae316debdf0ab2cf4953bef4b9dbf0a144423d4
Tahsin Özgür KOÇ <tahsinkoc6@gmail.com>
2025-06-21 09:38:27 +0300

    Login Form component created

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,14 @@
+import LoginForm from '@/components/LoginForm'
+import React from 'react'
+
+type Props = {}
+
+function page({ }: Props) {
+    return (
+        <div className=''>
+            <LoginForm />
+        </div>
+    )
+}
+
+export default page
---
+++ components/LoginForm.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react'
+
+type Props = {}
+
+function LoginForm({ }: Props) {
+    return (
+        <div className='w-full'>
+
+        </div>
+    )
+}
+
+export default LoginForm
